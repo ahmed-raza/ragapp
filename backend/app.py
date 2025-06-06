8e2cacd240cd7501a4677a82b2466d8e4e0e90f1
Baber Javed <baberjaved.rs@gmail.com>
2025-06-06 20:37:38 +0500

    Websocket connection which validates the user authenticates for now and sends back the received message

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from routes import routes
 from database import Base, engine
+from core.security import get_user_from_token
+from database import SessionLocal
+import asyncio
 
 app = FastAPI()
 
@@ -17,3 +20,33 @@ Base.metadata.create_all(bind=engine)
 
 for route in routes:
     app.include_router(route)
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    db = SessionLocal()
+    token = websocket.query_params.get("token")
+    if not token:
+        await websocket.close(code=1008)
+        return
+
+    try:
+        user = get_user_from_token(token, db)
+        await websocket.accept()
+        print(f"Authenticated WebSocket: {user.email}")
+    except HTTPException:
+        await websocket.close(code=1008)
+        return
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            words = data.split()
+
+            for word in words:
+                await websocket.send_text(word)
+                await asyncio.sleep(0.08)
+    except WebSocketDisconnect as e:
+        print(f"Client disconnected {e}")
+    except Exception as e:
+        print(f"WebSocket error: {e}")
+        await websocket.close()
